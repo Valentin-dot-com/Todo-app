8eba537ff06e46cdd99744c9862da548fcfa91c0
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-06-03 14:18:05 +0200

    feat: add localStorage functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,97 @@
-import { useState } from "react";
-import { Todo } from "./models/Todo";
-import { Todos } from "./components/Todos";
+import { useState } from 'react';
+import { Todo } from './models/Todo';
+import { Todos } from './components/Todos';
 
 export const App = () => {
+	const [todos, setTodos] = useState<Todo[]>( () => {
 
-  const [todos, setTodos] = useState<Todo[]>([
-    {
-      id: 1,
-      task: 'Damma & dammsuga hemma',
-      done: false,
-    },
-    {
-      id: 2,
-      task: 'Gör klart portfolio-hemsidan',
-      done: false,
-    },
-    {
-      id: 3,
-      task: 'Lär dig mer om react',
-      done: false,
-    },
-    {
-      id: 4,
-      task: 'Handla mat och snacks till helgen',
-      done: false,
-    },
-    {
-      id: 5,
-      task: 'Lämna in inlämningsuppgiften',
-      done: false,
-    },
-  ]);
+		const savedStorage = localStorage.getItem('todos');
+	
+		if (savedStorage) {
+			return JSON.parse(savedStorage)
+		}
+	
+		return [
+			{
+				id: 1,
+				task: 'Damma & dammsuga hemma',
+				done: false,
+			},
+			{
+				id: 2,
+				task: 'Gör klart portfolio-hemsidan',
+				done: false,
+			},
+			{
+				id: 3,
+				task: 'Lär dig mer om react',
+				done: false,
+			},
+			{
+				id: 4,
+				task: 'Handla mat och snacks till helgen',
+				done: false,
+			},
+			{
+				id: 5,
+				task: 'Lämna in inlämningsuppgiften',
+				done: false,
+			},
+		]
+	});
+	
+	// const [todos, setTodos] = useState<Todo[]>(
+	// 	JSON.parse(localStorage.getItem('todos')) || [
+	// 		{
+	// 			id: 1,
+	// 			task: 'Damma & dammsuga hemma',
+	// 			done: false,
+	// 		},
+	// 		{
+	// 			id: 2,
+	// 			task: 'Gör klart portfolio-hemsidan',
+	// 			done: false,
+	// 		},
+	// 		{
+	// 			id: 3,
+	// 			task: 'Lär dig mer om react',
+	// 			done: false,
+	// 		},
+	// 		{
+	// 			id: 4,
+	// 			task: 'Handla mat och snacks till helgen',
+	// 			done: false,
+	// 		},
+	// 		{
+	// 			id: 5,
+	// 			task: 'Lämna in inlämningsuppgiften',
+	// 			done: false,
+	// 		},
+	// 	]
+	// );
 
-  console.log(todos);
-  
+	console.log(todos);
 
-  const toggleTodoAsDone = (id: number) => {
-    setTodos(
-      todos.map((t) => {
-        if (t.id === id) {
-          return {...t, done: !t.done};
-        }
-        return t;
-      })
-    )
-  }
+	const toggleTodoAsDone = (id: number) => {
+		setTodos(
+			todos.map((t) => {
+				if (t.id === id) {
+					return { ...t, done: !t.done };
+				}
+				return t;
+			})
+		);
+	};
 
-  const deleteTodo = (id: number) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  }
+	const deleteTodo = (id: number) => {
+		setTodos(todos.filter((t) => t.id !== id));
+	};
+
+	localStorage.setItem('todos', JSON.stringify(todos));
 
 	return (
-    <>
-      <Todos todos={todos} toggleAsDone={toggleTodoAsDone} deleteTodo={deleteTodo} />
-    </>
-  );
+		<>
+			<Todos todos={todos} toggleAsDone={toggleTodoAsDone} deleteTodo={deleteTodo} />
+		</>
+	);
 };
