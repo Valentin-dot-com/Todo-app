3af95a58f1d1af100323d38c3b92b16cd79ed0e9
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-06-03 14:35:02 +0200

    fix: fix correct version of todo-list and LS

--- src/App.tsx
+++ src/App.tsx
@@ -3,72 +3,38 @@ import { Todo } from './models/Todo';
 import { Todos } from './components/Todos';
 
 export const App = () => {
-	const [todos, setTodos] = useState<Todo[]>( () => {
-
-		const savedStorage = localStorage.getItem('todos');
-	
-		if (savedStorage) {
-			return JSON.parse(savedStorage)
-		}
-	
-		return [
-			{
-				id: 1,
-				task: 'Damma & dammsuga hemma',
-				done: false,
-			},
-			{
-				id: 2,
-				task: 'Gör klart portfolio-hemsidan',
-				done: false,
-			},
-			{
-				id: 3,
-				task: 'Lär dig mer om react',
-				done: false,
-			},
-			{
-				id: 4,
-				task: 'Handla mat och snacks till helgen',
-				done: false,
-			},
-			{
-				id: 5,
-				task: 'Lämna in inlämningsuppgiften',
-				done: false,
-			},
-		]
-	});
-	
-	// const [todos, setTodos] = useState<Todo[]>(
-	// 	JSON.parse(localStorage.getItem('todos')) || [
-	// 		{
-	// 			id: 1,
-	// 			task: 'Damma & dammsuga hemma',
-	// 			done: false,
-	// 		},
-	// 		{
-	// 			id: 2,
-	// 			task: 'Gör klart portfolio-hemsidan',
-	// 			done: false,
-	// 		},
-	// 		{
-	// 			id: 3,
-	// 			task: 'Lär dig mer om react',
-	// 			done: false,
-	// 		},
-	// 		{
-	// 			id: 4,
-	// 			task: 'Handla mat och snacks till helgen',
-	// 			done: false,
-	// 		},
-	// 		{
-	// 			id: 5,
-	// 			task: 'Lämna in inlämningsuppgiften',
-	// 			done: false,
-	// 		},
-	// 	]
-	// );
+	const [todos, setTodos] = useState<Todo[]>(
+		JSON.parse(
+			localStorage.getItem('todos') ||
+				JSON.stringify([
+					{
+						id: 1,
+						task: 'Damma & dammsuga hemma',
+						done: false,
+					},
+					{
+						id: 2,
+						task: 'Gör klart portfolio-hemsidan',
+						done: false,
+					},
+					{
+						id: 3,
+						task: 'Lär dig mer om react',
+						done: false,
+					},
+					{
+						id: 4,
+						task: 'Handla mat och snacks till helgen',
+						done: false,
+					},
+					{
+						id: 5,
+						task: 'Lämna in inlämningsuppgiften',
+						done: false,
+					},
+				])
+		)
+	);
 
 	console.log(todos);
 
