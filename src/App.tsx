71a547e11ac72340b1d51228e3366b8f2a05da02
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-06-03 10:07:02 +0200

    feat: add list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { Todo } from "./models/Todo";
+
 export const App = () => {
-  return <></>;
-}
+
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: 1,
+      task: 'Damma & dammsuga hemma',
+      done: false,
+    },
+    {
+      id: 2,
+      task: 'Gör klart portfolio-hemsidan',
+      done: false,
+    },
+    {
+      id: 3,
+      task: 'Lär dig mer om react',
+      done: false,
+    },
+    {
+      id: 4,
+      task: 'Handla mat och snacks till helgen',
+      done: false,
+    },
+    {
+      id: 5,
+      task: 'Lämna in inlämningsuppgiften',
+      done: false,
+    },
+  ]);
+
+	return (
+    <>
+      
+    </>
+  );
+};
